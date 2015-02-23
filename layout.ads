e0f7b31c790c2cc10a195159d7804b6ad7361a2b
Alexandroni <jpalexandroni7@gmail.com>
2015-02-22 19:01:41 -0600

    Fixed Errors Type| Fixed Errors in functions/procedures calls

--- layout.ads
+++ layout.ads
@@ -8,12 +8,10 @@ package Layout is
       --how they connect to each other.
 
 
-      -- Types for an individual block
+   -- Types for an individual block
 
-
-   subtype Block_ID is Integer range 0 .. 40;
-   -- 0 indicates there is no such block
-
+   type Block_ID is range 1 .. 40;
+   -- Block ID Type 1 to 40
    type Block_Direction is (Normal, Reversed);
    -- Return the polarity of a block
 
@@ -21,25 +19,15 @@ package Layout is
    --Return a list of block with ID
 
 
-      -- Types for an individual turn out.
-
-
-   subtype Turnout_ID is Integer range 0 .. 26;
-   --0 means there is no ID
-
+   -- Types for an individual turn out.
+   type Turnout_ID is range 1 .. 26;
+   --Turnout ID 1 to 26
    type Turnout_Direction is (Left, Right);
    -- Return the polarity of a TurnOut
 
-   type Turnout_Joint is (Joint, Disjoint);
-   --Return Normal for not joint turnout, Joint for joint turn out
+   -- Types for an individual Sensor.
 
-   type Turnout_Set is (Left_set, Right_set);--------------------------
-
-
-      -- Types for an individual Sensor.
-
-
-   subtype Sensor_ID is Integer range 1 .. 51;
+   type Sensor_ID is range 1 .. 51;
 
    type Sensor_Status is (On, Off);
 
@@ -55,11 +43,27 @@ package Layout is
    type Terminator_Type is (Turnout, Block, Deadend);
    --Return the type of the end of a block, or turnout.
 
-
-
    -----------------------------------------------------------------------------
    --functions and procedures in our layout.
    -----------------------------------------------------------------------------
+
+   procedure Get_Next(Block_Num : in Block_ID;
+                      Direction : in Block_Direction;
+                      Terminator: out Terminator_Type);
+   --take a block ID, the direction, and return the terminator next to this block
+
+   procedure Get_Next_Block_ID (Block     : in Block_ID;
+                                Direction : in Block_Direction;
+                                Next_ID   : out Block_ID);
+   --take a block id, the direction, and return the next block id if there is a
+   --block next
+
+   procedure Get_Next_Turnout_ID (Block     : in Block_ID;
+                                  Direction : in Block_Direction;
+                                  Next_ID   : out Turnout_ID);
+   --take a block id, the directio, and return the next turnout id if there is a
+   --turnout next
+
    function Opposite (Direction : in Block_Direction) return Block_Direction;
    --function to return the opposite of a given block direction
 
@@ -70,7 +74,7 @@ package Layout is
     procedure Sensor_Blocks (Sensor  : in Sensor_ID;
                              Block1  : out Block_ID;
                              Block2  : out Block_ID);
-   --function to return the Block IDs that got separated by a given Sensor ID
+   --procedure to return the Block IDs that got separated by a given Sensor ID
 
    procedure Is_Force (Block_Num   : in Block_ID;
                        Direction   : in Block_Direction;
@@ -81,14 +85,13 @@ package Layout is
    procedure Who_is_Force (Block_Num       : in Block_ID;
                            Block_Dir       : in Block_Direction;
                            Turnout_Num     : out Turnout_ID;
-                           Turnout_Choice  : out Turnout_Set);
+                           Turnout_Choice  : out Turnout_Direction);
    --procedure to return the force turnout ID at the end of the
    --given block as well
    --as set up the direction for that turn outblock given block ID and Direction
 
-   procedure Is_Joint (Turnout_Num : in Turnout_ID;
-                       Direction   : in Turnout_Direction;
-                       Answer      : out Boolean);
+   function Is_Joint (Turnout_Num : in Turnout_ID;
+                      Direction   : in Turnout_Direction) return Boolean;
    --function answer whether a turn out is half of a joint turnout.
 
    procedure Who_is_Joint (Turnout_Num     : in Turnout_ID;
@@ -103,18 +106,8 @@ package Layout is
    --function that returns the next choice turnout ID of the given direction
    -- Block
 
-   -----------------------------------------------------------------------------
-   -- helper procedures, functions in our layout.
-   -----------------------------------------------------------------------------
-   procedure Get_Next(Block_Num : in Block_ID;
-                      Direction : in Block_Direction;
-                      ID        : out Integer;
-                      Terminator: out Terminator_Type);
-   --procedure that takes in Block_ID, and Direction of the Block, check the
-   --End_Of_Block array on the Direction end to see whether it is a block
-   -- or a choice.
-
-   procedure Is_On (Sensor : in Sensor_ID;
+   procedure Is_On (Block1 : in Block_ID;
+                    Block2 : in Block_ID;
                     Status : out Sensor_Status);
    --function return the status of a sensor
 end Layout;
